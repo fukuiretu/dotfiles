ed5d8196964b906aa5623949e9f496590c8a5ce0
fukuiretu <fukuiretu@gmail.com>
2017-01-10 14:45:12 +0900

    modify [ci skip]

---
+++ cookbooks/fish/files/Users/fukuiretu/.config/fish/conf.d/aliases.fish
@@ -0,0 +1,5 @@
+##### ruby #####
+alias be="bundle _1.10.6_ exec"
+alias bi="bundle install --path vendor/bundle"
+alias rs="rspec"
+alias ra="rake"
---
+++ cookbooks/fish/files/Users/fukuiretu/.config/fish/conf.d/peco.fish
@@ -0,0 +1,5 @@
+##### peco #####
+function fish_user_key_bindings
+    bind \cr peco_select_history
+    bind \cg peco_select_ghq_repository
+end
