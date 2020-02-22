e36f510633d1d7a4b34b3c2625d1e0e684561604
fukuiretu <fukuiretu@gmail.com>
2020-02-22 20:17:19 +0900

    tpmd導入

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,33 +6,37 @@ ln -fs ~/dotfiles/config/.tigrc  ~/.tigrc
 ln -fs ~/dotfiles/config/.vimrc  ~/.vimrc
 ln -fs ~/dotfiles/config/.tmux.conf  ~/.tmux.conf
 
-### homebrew 
-if !(type "brew" > /dev/null 2>&1); then
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
-
 ### homebrew-bundle 
 echo "start homebrew-bundle..."
 brew bundle
 
 ### anyenv
-echo "start anyenv..."
-anyenv install --init
-anyenv install nodenv
-anyenv install goenv
-exec $SHELL -l
+if [ ! -d "~/.anyenv" ]; then
+    echo "start anyenv..."
 
-mkdir -p $(anyenv root)/plugins
-if [ ! -d "$(anyenv root)/plugins/anyenv-update" ]; then
+    anyenv install --init
+    anyenv install nodenv
+    anyenv install goenv
+    exec $SHELL -l
+
+    mkdir -p $(anyenv root)/plugins
     git clone https://github.com/znz/anyenv-update.git $(anyenv root)/plugins/anyenv-update
 fi
 
 ### prezto
 if [ ! -d "${ZDOTDIR:-$HOME}/.zprezto" ]; then
+    echo "start prezto..."
     git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
     setopt EXTENDED_GLOB
-    for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+    for rcfile in "${ZDOTDIR:-$HOME}/.zprezto/runcoms/^README.md(.N)"; do
         ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
     done
+fi
+
+### tpm
+if [ ! -d "~/.tmux/plugins/tpm" ]; then
+    echo "start npm..."
+    mkdir -p ~/.tmux/plugins/
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
